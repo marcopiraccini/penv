76f5d27df2810b69a3930b58bdcdaba944d7953e
Marco <marco.piraccini@gmail.com>
2019-08-26 08:41:51 +0200

    updated readme and removed useless echo

--- penv.sh
+++ penv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#title        :connEnv.sh
-#description  :Develop env docker-based
+#title        : penv.sh
+#description  : Develop env docker-based
 #========================================================
 
 container_name=$(docker ps --format "{{.Names}}" | grep penv)
@@ -9,5 +9,4 @@ SUDO=$(if docker info 2>&1 | grep "permission denied" >/dev/null; then echo "sud
 socket=$($SUDO docker port $container_name 22)
 port="$(echo "$socket" | cut -d':' -f2)"
 sshArgs="$@ -Y -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p $port -i ~/.penv/penv-key"
-echo ssh $sshArgs localhost
 ssh $sshArgs localhost
